833142ce49cff7d79f8e2a7e98938788e347ccdc
rishika06 <rishika2306@gmail.com>
2022-06-28 11:26:10 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+app.listen(port, console.log(`server running on port ${port}`));
